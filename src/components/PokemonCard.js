f659f44f799fcefb0999a9e88253efdf9ce2faf8
awazienommy <coawazie@gmail.com>
2019-11-25 23:43:23 -0500

    done but image link of new pokemons not showing

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,39 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     clicked: false
+  }
+  this.handleClick = this.handleClick.bind(this)
+}
+
+handleClick(){
+  this.setState({
+    clicked: !this.state.clicked
+  })
+}
+
+
+
   render() {
+    const hp = this.props.pokemon.stats.find(stat => stat.name === 'hp')
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div onClick={this.handleClick} className="image">
+            <img alt="oh no!" src={!this.state.clicked ? this.props.pokemon.sprites.front : this.props.pokemon.sprites.back}/>
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              HP: {hp ? hp.value : null}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,9 +4,12 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    // console.log(this.props.pokemons[0])
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.props.pokemons.map(pokemon => <PokemonCard  pokemon={pokemon}/>)
+
+       }
       </Card.Group>
     )
   }
